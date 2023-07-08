db5d8f9f0273f897c32d8aad86a272c6d362501f
darwishdev <a.darwish.dev@gmail.com>
2023-07-09 00:17:39 +0300

    update vites.config to build the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,37 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    dts({ include: ['src/composables/*.ts'], })
+
   ],
+  build: {
+    cssCodeSplit: false,
+    lib: {
+      entry: ["./src/composables/index.ts"],
+      formats: ["es", "cjs"],
+      name: "FormkitBuilder",
+      fileName: (format, entry) => {
+        return format === `es` ? `${entry}.js` : `${entry}.cjs`
+      }
+    },
+    rollupOptions: {
+      external: ["vue",
+        "vue-router",
+      ],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
