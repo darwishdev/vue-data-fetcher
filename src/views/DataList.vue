601797785908a9df35113ba6af806e1c78a0e7a5
darwishdev <a.darwish.dev@gmail.com>
2023-07-09 00:05:20 +0300

    add end to end test for the datalist component

--- src/views/DataList.vue
+++ src/views/DataList.vue
@@ -5,9 +5,9 @@ import useDataFetcherList from '../composables/useDataFetcherList'
 const { responseData, loading, error } = useDataFetcherList<RolesListRequest, RolesListResponse>(apiClient.rolesList, {} as RolesListRequest);
 </script>
 <template>
-  <h2 v-if="loading"> loading</h2>
+  <h2 class="loading" v-if="loading">loading</h2>
   <h2 v-else-if="error">
     {{ error }}
   </h2>
-  <h2 v-else>{{ responseData }}</h2>
+  <h2 class="response" v-else>{{ JSON.stringify(responseData) }}</h2>
 </template>
