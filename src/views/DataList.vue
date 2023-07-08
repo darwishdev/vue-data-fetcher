43ba21d6ec90f940eb245f65b652471355e45944
darwishdev <a.darwish.dev@gmail.com>
2023-07-08 23:37:22 +0300

    create component and test the composable works as expected

---
+++ src/views/DataList.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import apiClient from '@/api/ApiMock';
+import type { RolesListResponse, RolesListRequest } from '@/api/ApiTypes'
+import useDataFetcherList from '../composables/useDataFetcherList'
+const { responseData, loading, error } = useDataFetcherList<RolesListRequest, RolesListResponse>(apiClient.rolesList, {} as RolesListRequest);
+</script>
+<template>
+  <h2 v-if="loading"> loading</h2>
+  <h2 v-else-if="error">
+    {{ error }}
+  </h2>
+  <h2 v-else>{{ responseData }}</h2>
+</template>
